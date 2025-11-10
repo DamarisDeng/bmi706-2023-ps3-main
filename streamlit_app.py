a8f2e5f1c42f9a66081ef69c2a5eea23fbeaeede
Damaris Deng <damarisdeng@outlook.com>
2025-11-09 23:05:36 -0500

    figures

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 ### P1.2 ###
 
-# Move this code into `load_data` function {{
+@st.cache_data
 def load_data():
     cancer_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/cancer_ICD10.csv").melt(  # type: ignore
         id_vars=["Country", "Year", "Cancer", "Sex"],
@@ -24,12 +24,6 @@ def load_data():
 
     df = df.groupby(["Country", "Year", "Cancer", "Age", "Sex"]).sum().reset_index()
     df["Rate"] = df["Deaths"] / df["Pop"] * 100_000
-    return df
-
-
-@st.cache_data
-def load_data():
-    df = load_data()
     return df
 
 
