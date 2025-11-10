fbb0256ae9b2b5f71e743bf8b29dc7704081670a
Damaris Deng <damarisdeng@outlook.com>
2025-11-09 22:55:44 -0500

    partial

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,37 +5,36 @@ import streamlit as st
 ### P1.2 ###
 
 # Move this code into `load_data` function {{
-cancer_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/cancer_ICD10.csv").melt(  # type: ignore
-    id_vars=["Country", "Year", "Cancer", "Sex"],
-    var_name="Age",
-    value_name="Deaths",
-)
-
-pop_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/population.csv").melt(  # type: ignore
-    id_vars=["Country", "Year", "Sex"],
-    var_name="Age",
-    value_name="Pop",
-)
-
-df = pd.merge(left=cancer_df, right=pop_df, how="left")
-df["Pop"] = df.groupby(["Country", "Sex", "Age"])["Pop"].fillna(method="bfill")
-df.dropna(inplace=True)
-
-df = df.groupby(["Country", "Year", "Cancer", "Age", "Sex"]).sum().reset_index()
-df["Rate"] = df["Deaths"] / df["Pop"] * 100_000
-
-# }}
-
-
-@st.cache
 def load_data():
-    ## {{ CODE HERE }} ##
-    df = ...  # remove this line
+    cancer_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/cancer_ICD10.csv").melt(  # type: ignore
+        id_vars=["Country", "Year", "Cancer", "Sex"],
+        var_name="Age",
+        value_name="Deaths",
+    )
+
+    pop_df = pd.read_csv("https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/population.csv").melt(  # type: ignore
+        id_vars=["Country", "Year", "Sex"],
+        var_name="Age",
+        value_name="Pop",
+    )
+
+    df = pd.merge(left=cancer_df, right=pop_df, how="left")
+    df["Pop"] = df.groupby(["Country", "Sex", "Age"])["Pop"].fillna(method="bfill")
+    df.dropna(inplace=True)
+
+    df = df.groupby(["Country", "Year", "Cancer", "Age", "Sex"]).sum().reset_index()
+    df["Rate"] = df["Deaths"] / df["Pop"] * 100_000
+    return df
+
+
+@st.cache_data
+def load_data():
+    df = load_data()
     return df
 
 
 # Uncomment the next line when finished
-# df = load_data()
+df = load_data()
 
 ### P1.2 ###
 
@@ -44,14 +43,14 @@ st.write("## Age-specific cancer mortality rates")
 
 ### P2.1 ###
 # replace with st.slider
-year = 2012
+year = st.slider("Year", min_value=2000, max_value=2018, value=2018, step=1)
 subset = df[df["Year"] == year]
 ### P2.1 ###
 
 
 ### P2.2 ###
 # replace with st.radio
-sex = "M"
+sex = st.radio(label="Sex", options=["M", "F"], index=0)
 subset = subset[subset["Sex"] == sex]
 ### P2.2 ###
 
@@ -68,13 +67,14 @@ countries = [
     "Thailand",
     "Turkey",
 ]
+countries = st.multiselect("Country", options=countries, default=countries)
 subset = subset[subset["Country"].isin(countries)]
 ### P2.3 ###
 
 
 ### P2.4 ###
 # replace with st.selectbox
-cancer = "Malignant neoplasm of stomach"
+cancer = st.selectbox("Cancer", options=df["Cancer"].unique(), index=0)
 subset = subset[subset["Cancer"] == cancer]
 ### P2.4 ###
 
@@ -91,11 +91,13 @@ ages = [
     "Age >64",
 ]
 
-chart = alt.Chart(subset).mark_bar().encode(
+chart = alt.Chart(subset).mark_rect().encode(
     x=alt.X("Age", sort=ages),
-    y=alt.Y("Rate", title="Mortality rate per 100k"),
-    color="Country",
-    tooltip=["Rate"],
+    y=alt.Y("Country"),
+    color=alt.Color("Rate",
+                     title="Mortality rate per 100k",
+                     scale=alt.Scale(type='log', domain=[0.01, 1000], clamp=True)),
+    tooltip=["Country", "Age", "Rate"],
 ).properties(
     title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
 )
